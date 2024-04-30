41ba4c96b65b99ee1e373ef2f97ae9769972eaea
adithya4080 <adithyas712@gmail.com>
2024-04-30 15:16:44 +0530

    update

--- src/components/Navbar/NavbarBottom.jsx
+++ src/components/Navbar/NavbarBottom.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Img, Text } from "../../components";
 import Grocery from "../../assets/images/food.png"
 import Phone from "../../assets/images/Phone.png";
 import Sofa from "../../assets/images/Sofa.png";
@@ -14,7 +13,9 @@ function NavbarBottom() {
                     <div className="h-[2px] w-[8%] bg-blue-600" />
                     <div className="flex gap-7 pt-4">
                         <div className="flex self-end items-center gap-[7px]">
-                            <Img src="images/img_ei_star.svg" alt="image" className="h-[16px] w-[16px]" />
+                        <div className='w-[30px]'>
+                            <img src="images/img_ei_star.svg" alt="image" className="w-full" />
+                        </div>
                             <a href="#">
                                 <h3 className="font-bold text-sm text-blue-600">
                                     Top Offers
@@ -22,7 +23,7 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <img src={Grocery} alt="image" className="h-[16px] w-[16px]" />
+                            <div className='w-[30px]'><img src={Grocery} alt="image" className="w-full" /></div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Grocery
@@ -30,7 +31,7 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <img src={Phone} alt="image" className="h-[16px] w-[16px]" />
+                            <div className='w-[30px]'><img src={Phone} alt="image" className="w-full" /></div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Mobiles
@@ -38,7 +39,9 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <img src={Sofa} alt="image" className="h-[18px] w-[18px]" />
+                            <div className='w-[30px]'>
+                                <img src={Sofa} alt="image" className="w-full" />
+                            </div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Home
@@ -46,7 +49,9 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <img src={Electronics} alt="image" className="h-[18px] w-[18px]" />
+                            <div className='w-[30px]'>
+                                <img src={Electronics} alt="image" className="w-full" />
+                            </div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Electronics
@@ -54,7 +59,9 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <Img src="images/img_shoe.png" alt="shoe_one" className="w-[18px] h-[18px]" />
+                            <div className='w-[30px]'>
+                                <img src="images/img_shoe.png" alt="shoe_one" className="w-full" />
+                            </div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Fashion
@@ -62,7 +69,9 @@ function NavbarBottom() {
                             </a>
                         </div>
                         <div className="flex self-end items-center gap-[7px]">
-                            <img src={Beauty} alt="image" className="h-[18px] w-[18px]" />
+                            <div className='w-[30px]'>
+                                <img src={Beauty} alt="image" className="w-full" />
+                            </div>
                             <a href="#">
                                 <h3 className="font-medium text-sm text-black-600">
                                     Beauty & Accessories
