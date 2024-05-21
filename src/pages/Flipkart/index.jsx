35946f20b96b91ffa10c3df81f2806ea3774b6fd
adithya4080 <adithyas712@gmail.com>
2024-05-21 12:23:32 +0530

    update

--- src/pages/Flipkart/index.jsx
+++ src/pages/Flipkart/index.jsx
@@ -7,6 +7,7 @@ import Spotlight from "../../components/Screen/Home/Spotlight"
 import FeaturedProduct from "components/Screen/Home/FeaturedProduct";
 import Ad from "components/Screen/Home/Ad";
 import Products from "components/Screen/Home/Products";
+import Categories from "components/Screen/Home/Categories";
 
 
 export default function FlipkartPage() {
@@ -24,6 +25,7 @@ export default function FlipkartPage() {
       <Spotlight />
       <FeaturedProduct />
       <Ad />
+      <Categories />
       <Products />
         <div>
 
@@ -35,97 +37,8 @@ export default function FlipkartPage() {
                   <div className="justify-center h-[3777px] w-full left-0 bottom-0 right-0 top-0 m-auto bg-white-A700 absolute" />
                 
                   
-                  <div className="flex md:flex-col justify-center items-center w-full bottom-[36%] right-0 left-0 m-auto absolute">
-                    <Img
-                      src="images/img_image_15.png"
-                      alt="imagefifteen"
-                      className="self-end w-[27%] md:w-full z-[1] object-cover"
-                    />
-                    <div className="h-[456px] md:w-full ml-[-409px] flex-1 relative md:flex-none">
-                      <div className="w-full top-0 right-0 left-0 m-auto absolute">
-                        <div className="flex md:flex-col justify-between gap-5">
-                          <div className="h-[433px] w-[24%] relative">
-                            <div className="justify-center h-[433px] w-full left-0 bottom-0 right-0 top-0 m-auto bg-yellow-200 absolute rounded-[10px]" />
-                            <Heading
-                              size="4xl"
-                              as="h2"
-                              className="left-[11%] top-[8%] m-auto !text-black-900 tracking-[0.30px] absolute"
-                            >
-                              Fashion
-                            </Heading>
-                          </div>
-                          <div className="h-[433px] w-[24%] bg-green-A100 rounded-[10px]" />
-                        </div>
-                      </div>
-                      <div className="h-[433px] w-[33%] left-[26%] top-0 m-auto bg-red-100_01 absolute rounded-[10px]" />
-                      <div className="h-[433px] w-[32%] right-[23%] top-0 m-auto bg-blue-50_01 absolute rounded-[10px]" />
-                      <Img
-                        src="images/img_image_17.png"
-                        alt="imageseventeen"
-                        className="h-[302px] w-[34%] bottom-0 right-[23%] m-auto object-cover absolute"
-                      />
-                    </div>
-                  </div>
-                  <Img
-                    src="images/img_image_16.png"
-                    alt="imagesixteen"
-                    className="h-[322px] w-[28%] bottom-[35%] left-[33%] m-auto object-cover absolute"
-                  />
-                  <div className="flex flex-col w-[90%] gap-[86px] top-[10%] right-0 left-0 m-auto absolute">
-                
-                    <div>
-                      <div className="flex justify-between items-start gap-5">
-                        <div className="h-[260px] w-[18%] relative">
-                          <div className="flex flex-col items-start justify-center w-max h-full pb-1 gap-[214px] left-0 bottom-0 right-0 top-0 px-1 m-auto bg-green-50 absolute rounded-[10px]">
-                            <div className="h-[20px] w-[29%] bg-green-A700 rounded-[10px]" />
-                            <div className="h-[13px] w-[33%] mb-2 ml-[7px] relative">
-                              <Text
-                                size="xs"
-                                as="p"
-                                className="justify-center w-max left-0 bottom-0 right-0 top-0 m-auto !text-black-900 absolute"
-                              >
-                                <span className="text-gray-600">₹1,699</span>
-                                <span className="text-black-900">&nbsp;</span>
-                                <span className="text-green_A700_01">65% off</span>
-                              </Text>
-                              <div className="h-px w-[32%] left-[7%] top-[42%] m-auto rotate-[-180deg] bg-gray-600 absolute" />
-                            </div>
-                          </div>
-                          <Heading
-                            size="md"
-                            as="p"
-                            className="w-[94%] bottom-[17%] right-0 m-auto !text-black-900 absolute"
-                          >
-                            <span className="text-blue_gray-400 font-medium">
-                              <>
-                                Sponsored
-                                <br />
-                              </>
-                            </span>
-                            <span className="text-black-900 font-medium">STREE MANTRA </span>
-                            <span className="text-black-900 font-medium">
-                              <>
-                                Women <br />
-                                Printed Georgette, Crepe <br />
-                                A-line Kurta{" "}
-                              </>
-                            </span>
-                            <span className="text-black-900 font-medium">
-                              <>
-                                Grey
-                                <br />
-                              </>
-                            </span>
-                          </Heading>
-                        </div>
-                        <a href="#" className="mt-[91px]">
-                          <Text as="p" className="!text-gray-600">
-                            View More
-                          </Text>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+                  
+                  
                   <div className="h-[463px] w-[83%] bottom-[2%] right-0 left-0 m-auto absolute">
                     <div className="flex justify-center w-full h-full left-0 bottom-0 right-0 top-0 p-[11px] m-auto bg-gradient1 absolute rounded-[30px]">
                       <div className="flex md:flex-col justify-between items-center w-[94%] mt-[17px] mb-[35px] gap-5">
